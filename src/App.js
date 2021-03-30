3f5f9da03cee6466b6d7cbafee795ab2c928c981
Szymon Waliczek <waliczek.szymon@gmail.com>
2021-03-30 01:16:23 +0100

    Initial setup and file structure for application.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./routes/home/Home";
+import Login from "./routes/login/Login";
+import Calendar from "./routes/calendar/Calendar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/calendar">
+          <Calendar />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
