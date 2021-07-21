86bf6624b95ef028988603984a7a04f07aa353e9
Szymon Waliczek <waliczek.szymon@gmail.com>
2021-07-21 23:57:23 +0000

    add feedback when content is loading, and in case of error

--- src/routes/workshop/Workshop.js
+++ src/routes/workshop/Workshop.js
@@ -7,7 +7,13 @@ import { DragDropContext } from "react-beautiful-dnd";
 import apiClient from "../../service/api/api";
 import "./Workshop.css";
 
-const Workshop = ({ setIsLoading, setLoggedIn, setLoginErrorMsg, toast }) => {
+const Workshop = ({
+  // isLoading,
+  setIsLoading,
+  setLoggedIn,
+  setLoginErrorMsg,
+  toast,
+}) => {
   // when page oppened check if user logged in, if not redirect to login page
   const { isLoading, SpinnerComponent } = IsLoggedInLogic(
     setLoginErrorMsg,
@@ -15,62 +21,88 @@ const Workshop = ({ setIsLoading, setLoggedIn, setLoginErrorMsg, toast }) => {
     setLoggedIn
   );
 
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState([]);
+  const [loadingError, setLoadingError] = useState(false);
 
   ondragend = (result) => {
     console.log(result);
   };
 
   const getWorkshop = () => {
+    setIsLoading(true);
+    setLoadingError(false);
     let url = "/api/v1/workshop";
-    // setIsLoading(true);
+
+    // setTimeout(() => {
     apiClient
       .get(url)
       .then((response) => {
-        // setIsLoading(false);
+        setIsLoading(false);
         console.log(response.data.data);
         setData(response.data.data);
 
         // setData(response.data.data);
       })
       .catch((err) => {
-        // setIsLoading(false);
+        setIsLoading(false);
+        setLoadingError(true);
         // setErrorMsg("I was unable to load assets :-(");
-        // toast.error(
-        //   <div className="toast_wrap">
-        //     Error while loading assets. {JSON.stringify(err.data.message)}
-        //   </div>
-        // );
+        toast.error(
+          <div className="toast_wrap">
+            Error while loading data.
+            <p>{JSON.stringify(err)}</p>
+          </div>
+        );
         console.log(err.data);
       });
+    // }, 5000);
   };
 
   useEffect(() => {
     getWorkshop();
-    console.log(initialData);
+    // console.log(initialData);
   }, []);
 
-  return (
+  return data.length !== 0 && !loadingError ? (
     <DragDropContext onDragEnd={ondragend}>
       <div className="scroll">
         <Row
           className="p-0 m-0 row-cols-sm-2 row-cols-md-4 row-cols-xl-5 row-cols-xxl-6 h-100"
           // style={{ minWidth: "1248px" }}
         >
-          {data.columnOrder.map((columnId) => {
-            const column = data.columns[columnId];
-            const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+          {data.length !== 0 && !loadingError ? (
+            data.columnOrder.map((columnId) => {
+              const column = data.columns[columnId];
+              const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
 
-            // return <Column key={column.id} column={column} tasks={tasks} />;
-            return (
-              <Col className="p-1 h-50 workshop-col-main">
-                <Column key={column.id} column={column} tasks={tasks} />
-              </Col>
-            );
-          })}
+              // return <Column key={column.id} column={column} tasks={tasks} />;
+              return (
+                <Col className="p-1 h-50 workshop-col-main">
+                  <Column key={column.id} column={column} tasks={tasks} />
+                </Col>
+              );
+            })
+          ) : (
+            <></>
+          )}
         </Row>
       </div>
     </DragDropContext>
+  ) : loadingError ? (
+    <div className="div-center">
+      <Row className="row-cols-12 text-center">
+        <Col>
+          <div className="my-4">
+            An unknown error occured while loading the data... :-(
+          </div>
+          <Button variant="info" onClick={() => getWorkshop()}>
+            Reload
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  ) : (
+    <></>
   );
 };
 
