a224f199a98d77a666b43b3be14c111c86501ccc
Szymon Waliczek <waliczek.szymon@gmail.com>
2021-11-04 22:38:44 +0000

    Add edit/save button to booking page and job ID field

--- src/routes/booking/BookingPage.js
+++ src/routes/booking/BookingPage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, Fragment } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Form, Row, Col } from "react-bootstrap";
+import { Container, Form, Row, Col, Button, Spinner } from "react-bootstrap";
 import apiClient from "../../service/api/api";
 import IsLoggedInLogic from "../../components/IsLoggedInLogic";
 import { Formik } from "formik";
@@ -13,9 +13,10 @@ const BookingPage = ({
   setLoginErrorMsg,
 }) => {
   const { uuid } = useParams(); // parameter from url
+  const [initialValues, setInitialValues] = useState();
   const [assets, setAssets] = useState([]);
   const [customers, setCustomers] = useState([]);
-  const [initialValues, setInitialValues] = useState();
+  const [editToggled, setEditToggled] = useState(false);
 
   // when page oppened check if user logged in, if not redirect to login page
   const {} = IsLoggedInLogic(setLoginErrorMsg, setIsLoading, setLoggedIn);
@@ -54,11 +55,31 @@ const BookingPage = ({
       .get(url)
       .then((response) => {
         console.log(response.data.data);
-        
+
         setCustomers(response.data.data);
       })
       .catch((err) => {
         console.log("error:", err);
+      });
+  };
+
+  // Submit function
+  const handleSubmit = (values, actions) => {
+    let url = "/api/v1/event_uuid/" + initialValues.event_id;
+    console.log("VALUES: ", values);
+    // setWaitingResponse(true);
+
+    apiClient
+      .patch(url, values)
+      .then(() => {
+        setEditToggled(false);
+        actions.resetForm();
+        toast.success("Booking updated successfully");
+      })
+      .catch((err) => {
+        console.log("error:", err);
+        // setWaitingResponse(false);
+        toast.error(err.statusText + " - Event NOT updated!");
       });
   };
 
@@ -71,12 +92,11 @@ const BookingPage = ({
   return initialValues ? (
     <Formik
       initialValues={initialValues}
-      // enableReinitialize={true}
       // validationSchema={reviewShema}
       onSubmit={(values, actions) => {
         console.log("RUNNING SUBMIT!");
-        // handleSubmit(values);
-        actions.setSubmitting(false);
+        actions.setSubmitting(true);
+        handleSubmit(values, actions);
       }}
     >
       {(props) => (
@@ -84,9 +104,65 @@ const BookingPage = ({
           <Form onSubmit={props.handleSubmit}>
             <Row className="row">
               <Container className="scroll py-3">
-                <h2>Booking details:</h2>
+                <Row>
+                  <Col className="col-8">
+                    <h2>Booking details:</h2>
+                  </Col>
+                  <Col className="col-4 text-end">
+                    <Button
+                      disabled={props.isSubmitting}
+                      variant={
+                        !editToggled
+                          ? "info"
+                          : !props.dirty
+                          ? "light"
+                          : "success"
+                      }
+                      onClick={() => {
+                        if (!editToggled) {
+                          setEditToggled(true);
+                        } else {
+                          if (props.dirty) {
+                            props.submitForm();
+                          } else {
+                            setEditToggled(false);
+                          }
+                        }
+                      }}
+                    >
+                      <Spinner
+                        as="span"
+                        animation="border"
+                        size="sm"
+                        className={`me-2 ${props.isSubmitting ? "" : "d-none"}`}
+                        role="status"
+                        aria-hidden="true"
+                      />
+                      {!editToggled ? "Edit" : props.dirty ? "Save" : "Cancel"}
+                    </Button>
+                  </Col>
+                </Row>
                 Booking page for event id: {uuid}
               </Container>
+
+              {/* Job ID */}
+              <Form.Group as={Col} md="4">
+                <Form.Label column className="text-md-end">
+                  Job ID
+                </Form.Label>
+                <Form.Control
+                  name="id"
+                  placeholder="Job ID"
+                  readOnly={true}
+                  plaintext={true}
+                  disabled={!editToggled}
+                  type="text"
+                  title="Job ID, not editable data"
+                  onChange={props.handleChange("others")}
+                  defaultValue={props.values.id}
+                  isInvalid={!!props.errors.id}
+                />
+              </Form.Group>
 
               {/* Reg */}
               <Form.Group as={Col} md="4" controlId="formReg">
@@ -98,6 +174,8 @@ const BookingPage = ({
                   id="asset-typeahead"
                   labelKey="reg"
                   // disabled
+                  plaintext={!editToggled}
+                  disabled={!editToggled}
                   onChange={(selected) => {
                     // check if selection made, if not update formik to throw error.
                     if (selected.length > 0) {
@@ -125,6 +203,8 @@ const BookingPage = ({
                 <Typeahead
                   id="customer-typeahead"
                   labelKey="customer_name"
+                  plaintext={!editToggled}
+                  disabled={!editToggled}
                   onChange={(selected) => {
                     // check if selection made, if not update formik to throw error.
                     if (selected.length > 0) {
